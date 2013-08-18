6eafb848c50fa05da351264cb2d1545ab249f784
Nick Payne <slashnick@gmail.com>
2013-08-18 13:27:22 +0100

    Added clj-drone

--- src/noisy/core.clj
+++ src/noisy/core.clj
@@ -1,7 +1,12 @@
 (ns noisy.core
-  [:use overtone.live])
+  [:use overtone.live clj-drone.core])
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(drone-initialize)
+(drone :take-off)
+(Thread/sleep 10000)
+(drone :land)
